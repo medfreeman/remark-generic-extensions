d84a48750d2963ae271fcea0d5f0e4cd5a41f51f
Mehdi Lahlou <mehdi.lahlou@free.fr>
2017-05-24 16:12:03 +0200

    change: add `debug` ruleId param to debug function
    Add `ruleID` param to warning function

--- src/utils/eat.js
+++ src/utils/eat.js
@@ -2,12 +2,12 @@ function vfileFail(message) {
   this.file.fail(message , this.now())
 }
 
-function vfileWarning(message) {
-  this.file.message(message , this.now())
+function vfileWarning(message, ruleId = "") {
+  this.file.message(message , this.now(), ruleId)
 }
 
 function vfileDebug(message) {
-  this::vfileWarning(message)
+  this::vfileWarning(message, "debug")
 }
 
 export { vfileFail, vfileWarning, vfileDebug }
