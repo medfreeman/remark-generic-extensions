fb5cd62499aae64bcb9160145f0d5d8678122e91
Mehdi Lahlou <mehdi.lahlou@free.fr>
2017-07-14 14:27:49 +0200

    chore: remove unnecessary escaping, eslint compliance

--- src/utils/string.js
+++ src/utils/string.js
@@ -1,6 +1,6 @@
 // https://stackoverflow.com/a/3561711
 const escapeRegExp = function() {
-  return this.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&")
+  return this.replace(/[-/\\^$*+?.()|[\]{}]/g, "\\$&")
 }
 
 export { escapeRegExp }
