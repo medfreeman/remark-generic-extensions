a29e031da8ae4e2e1b4bcdd0a2446d3170ebba4d
Mehdi Lahlou <mehdi.lahlou@free.fr>
2017-07-16 18:52:17 +0200

    fix(examples): only import component js, without css

--- examples/react-toolbox-icon/src/TooltipIcon.jsx
+++ examples/react-toolbox-icon/src/TooltipIcon.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Button } from "react-toolbox/lib/button"
+import { Button } from "react-toolbox/lib/button/Button"
 import Tooltip from "react-toolbox/lib/tooltip"
 
 const TooltipButton = Tooltip(Button)
