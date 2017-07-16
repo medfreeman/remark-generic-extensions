e364c90ebe9f199ee2df89ea469ba43de2a43155
Mehdi Lahlou <mehdi.lahlou@free.fr>
2017-07-16 20:02:13 +0200

    fix(examples): fix TooltipIcon imports

--- examples/react-toolbox-icon/src/TooltipIcon.jsx
+++ examples/react-toolbox-icon/src/TooltipIcon.jsx
@@ -1,18 +1,15 @@
 import React from "react"
 import Button from "react-toolbox/lib/button/Button"
-import Tooltip from "react-toolbox/lib/tooltip/Tooltip"
+import Tooltip from "react-toolbox/lib/tooltip"
 
 const TooltipButton = Tooltip(Button)
-
-const styles = {
-}
 
 const TooltipIcon = props => {
 
   const { floating, ...otherProps } = props
 
   return (
-    <TooltipButton style={styles} { ...props }/>
+    <TooltipButton { ...props }/>
   )
 }
 
